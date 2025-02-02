c4428555cbd2d6736c4e0f82dc3dbf5d0a90264f
johnwallacegerrard <levijohnbailey@gmail.com>
2025-02-02 16:46:35 -0500

    create open and close mdoal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
-const 
+const mentorModal = document.querySelector("#mentor-modal");
+const registrationButton = document.querySelector(".registration__btn");
+const modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__close-btn");
+
+function openModal(modal) {
+  console.log(modal);
+  modal.classList.add("modal_open");
+  //modal.addEventListener("click", handleOverlay);
+}
+
+const closeModal = (modal) => {
+  modal.classList.remove("modal_open");
+  //modal.removeEventListener("click, handleOverlay");
+};
+
+registrationButton.addEventListener("click", () => {
+  openModal(mentorModal);
+});
+modalCloseButton.addEventListener("click", () => {
+  closeModal(modal);
+});
